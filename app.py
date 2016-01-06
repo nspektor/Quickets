3e73e69cc84e9da4476b129753e1ac9ef03705e6
nspektor <nellie.spektor@gmail.com>
2016-01-06 12:01:22 -0500

    app.py stuff

--- app.py
+++ app.py
@@ -6,23 +6,19 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 @app.route("/home",methods=["GET","POST"])
 def home():
-    if request.method == "GET":
-        return render_template("home.html")
-    else:
-        payload = {'X-AMC-Vendor-Key':'451EB6B4-E2FD-412E-AF07-CA640853CDC3'}
-        r = requests.post("https://api.amctheatres.com/v2/movies",data=payload)
-        print r
+   # if request.method == "GET":
+    return render_template("home.html")
+#    else:
+ #       payload = {'X-AMC-Vendor-Key':'451EB6B4-E2FD-412E-AF07-CA640853CDC3'}
+ #       r = requests.post("https://api.amctheatres.com/v2/movies",data=payload)
+  #      print r
 
-@app.route("/login",methods=["GET","POST"])
-def login():
-    if request.method == "GET":
-        return render_template("login.html")
-    else:
+#@app.route("/login",methods=["GET","POST"])
+#def login():
+ #   if request.method == "GET":
+  #      return render_template("login.html")
+   # else:
         
-
-payload = {'X-AMC-Vendor-Key':'451EB6B4-E2FD-412E-AF07-CA640853CDC3'}
-r = requests.post("https://api.amctheatres.com/v2/movies",data=payload)
-print r
     
 if __name__=="__main__":
     app.debug = True
