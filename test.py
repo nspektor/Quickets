ec539440f7d2ba5cd25c9d4b189c7b20d6e99061
masha <maryged@gmail.com>
2016-01-08 12:13:28 -0500

    yay-pi

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 import requests
 
 
-payload = {'X-AMC-Vendor-Key':'451EB6B4-E2FD-412E-AF07-CA640853CDC3'}
-r = requests.get("https://api.amctheatres.com/v2/movies/views/now-playing",data=payload)
-print r
+headers = {'X-AMC-Vendor-Key':'451EB6B4-E2FD-412E-AF07-CA640853CDC3'}
+r = requests.get("https://api.amctheatres.com/v2/movies/views/now-playing",headers=headers)
+q=r.json()
+#print q.keys()
+#print q['_embedded']
+embed=q['_embedded']
+movies=embed['movies']
+#print movies[0]['name']
+
+movielist=q['_embedded']['movies']
+for movie in movielist:
+    print movie['name']
+#print movielist
+#print r
