a35166df9d1df8a1339fc18a061e8ce16a10a9d8
Michael Mattson <mhmattson@gmail.com>
2016-01-12 12:22:03 -0500

    starting database

---
+++ init.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+
+"""THIS FILE IS USED TO CREATE THE BACKEND OF THE PROJECT, IT CREATES THE
+DATABASE FILE AND CREATES ITS TABLES. ALL FUNCTIONS REGARDING CHANGING THE
+DATABASE IS LOCATED AT database.py"""
+
+conn = sqlite3.connect("infos.db")
+
+c = conn.cursor()
+
+q = "CREATE TABLE %s (%s)" # format string for creating tables,
+                           # first formatter = name
+                           # second formatter = arguments
+
+c.execute(q % ("users", "username TEXT, password TEXT")) # NOTE: hex string will do fine for hash
+
+c.execute(q % ("stories", "id INTEGER, sentence TEXT, author TEXT, time INTEGER"))
+
+c.execute(q % ("favorites", "id INTEGER, username TEXT"))
+
+
+
+conn.commit()
