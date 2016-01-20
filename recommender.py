0669c03c488d1ef0c4107f57a0f69e1aec1508ff
masha <maryged@gmail.com>
2016-01-20 12:33:23 -0500

    new beginninga

--- recommender.py
+++ recommender.py
@@ -1,4 +1,5 @@
 import sqlite3
+import utils
 
 #8b1eef7d82f877bf8e0482fa5aae709c - API TMDB                                  
 
@@ -11,33 +12,37 @@ import sqlite3
    call the now playing fxn from utils
 '''
 
-def display_favorites():
 '''
 display that user's favorites list
 '''
+#def display_favorites():
 
-def delete():
 '''   
 delete a movie from the user's favorite list
 '''
+#def delete():
 
-def add(movie):
 '''
 input: movie 
 adds movie to user's favorite list
 '''
+#def add(movie):
 
-def recommend(favorites):
 '''
-recommends one of the now playing movies based on the genres of the user's 
-favorites.
-   recommends random playing movie of the genre of whichever genre is most
-   common in the users favorites list 
+recommends one of the now playing movies based on the user's favorite genres
 If the user clicks "nope" takes that movie out of the running for
 the recommended movie. note: delete the maybe later button, its useless
 If there are no more movies and the person rejected all of them, error message
 '''
-
+def recommend(favorites):
+    listSize=len(favorites)
+    uniqs=set(favorites)
+    for el in uniqs:
+        print el
+    
 '''
 favorites database has to have movie name and genre
 '''
+
+favgens=['action', 'comedy', 'horror', 'horror']
+recommend(favgens)
